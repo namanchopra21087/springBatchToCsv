e85faec483a6d6bb53087d90ef3c6225f00affc4
naman <namanchopra21087@gmail.com>
2018-06-10 22:15:29 +0530

    Angular First Heroes Project

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Naman Fisrt Angular App';
+  title = 'Naman First Angular App';
 }
